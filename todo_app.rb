46696a46c31b93573df7597921dc38144844a7fa
Eduardo Braga <ebraga@thoughtworks.com>
2015-07-06 01:43:36 -0300

    adicionado data mapper, modificados estilos, adicionadas novas views para tratar de tarefas, criado novo verbo http

--- todo_app.rb
+++ todo_app.rb
@@ -1,10 +1,29 @@
 require 'sinatra'
+require 'data_mapper'
+
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/development.db")
+
+class Tarefa
+  include DataMapper::Resource
+
+  property :id,           Serial
+  property :content,      String, :required => true
+  property :completed_at, DateTime
+end
+
+DataMapper.finalize.auto_upgrade!
 
 get '/' do
+  @tarefas = Tarefa.all
   erb :index
 end
 
-get '/:tarefa' do
-  @tarefa = params[:tarefa].gsub!('-', ' ').capitalize
-  erb :tarefa
+post '/' do
+  Tarefa.create params[:tarefa]
+  redirect to('/')
+end
+
+get '/tarefas/:id' do |id|
+  @tarefa = Tarefa.get!(id)
+  erb :'tarefas/show'
 end
